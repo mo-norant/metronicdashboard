c8d827553f404646886718c41c2afc7a99f41f98
Mo <info@norant.be>
2018-05-22 11:24:05 +0200

    new frontend

---
+++ src/app/oogstkaart/oogstkaart.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShopComponent } from './shop/shop.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ShopComponent]
+})
+export class OogstkaartModule { }
